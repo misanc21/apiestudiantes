853bbbd203e2e6ae2d7f0b866f44fae277f459f0
misanc21 <misanc21@gmail.com>
2020-07-11 14:44:33 -0500

    https

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const mongoose = require ('mongoose')
+const express = require ('express')
+const app = express()
 
 /**Definir el modelo de estudiante */
 
@@ -21,15 +23,59 @@ const Student = mongoose.model('students',{
         min: 18,
     }
 })
+/**Midleware */
+app.use(express.json())
+
+/**Endpoints */
+app.get('/students', async (request, response) =>{
+    const allStudents  = await Student.find()
+
+    response.json({
+        success:true,
+        data: {
+            students : allStudents
+        }
+    })
+})
+
+app.post('/students', async (request,response) =>{
+    const studentInfo = request.body
+    const newStudent = await Student.create(studentInfo)
+    response.json({
+        success:true,
+        data:{
+            student: newStudent
+        }
+    })
+})
+
+app.delete('/students/:id', async (request, response)=>{
+    try{
+        const id = request.params.id
+        const studentDeleted = await Student.findByIdAndDelete(id)
+        response.json({
+            success:true,
+            data:{
+                student: studentDeleted
+            }
+        })
+    }catch(error){
+        response.status = 400
+        response.json({
+            success:false,
+            error: error.message
+        })
+    }
+})
+
+
+
 
 mongoose.connect('mongodb+srv://misanc21:Qwerty123@pruebaestudiantes.8o2ue.mongodb.net/school', 
     {useNewUrlParser: true, 
     useUnifiedTopology: true}, () =>{
-        console.log("rfrrf")
-        Student.create({
-            name: 'Misael',
-            course: 'ingles',
-            age:30
+        app.listen(8080, () =>{
+            console.log('Server is ready')
         })
     });
 
